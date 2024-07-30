eb2c8f61ae78edb93c391a56629c2ebbb08dbb03
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-30 15:51:52 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@real-state.t2tpfhs.mongodb.net/?retryWrites=true&w=majority&appName=Real-state`
+  )
+  .then(() => {
+    console.log(`connected to mongodb`);
+  })
+  .catch(() => {
+    console.log(err);
+  });
+
+// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
 const app = express();
 const port = 3000;
-
+//QE8RxdBYx7yhmtaw
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
