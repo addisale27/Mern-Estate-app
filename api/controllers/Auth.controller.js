3688cc4150be634ea86c37b820ab4b5bafe76e5a
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-30 20:24:19 +0300

    create a middle ware

--- api/controllers/Auth.controller.js
+++ api/controllers/Auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
-export const signUp = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signUp = async (req, res, next) => {
   const { username, password, email } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, password: hashedPassword, email });
@@ -8,6 +9,6 @@ export const signUp = async (req, res) => {
     await newUser.save();
     res.status(201).json("user created successfully!");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -29,3 +29,13 @@ app.listen(port, () => {
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+//midleware for handling the error
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error!";
+  return res.status(statusCode).json({
+    message,
+    success: false,
+    statusCode,
+  });
+});
