43b520302e792dbd01e6a257e6eb13c3df55bb18
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-30 22:10:24 +0300

    complete signup page

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.listen(port, () => {
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-//midleware for handling the error
+//middleware for handling the error
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error!";
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,34 +1,69 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/auth/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        throw new Error(data.message);
+      }
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setError(error.message);
+      console.Error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+  console.log(error);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold my-7 text-center"> Sign Up</h1>
-      <form
-        action="
-      "
-        className="flex flex-col   gap-4"
-      >
+      <form onSubmit={handleSubmit} className="flex flex-col   gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? `loading` : `Sign Up`}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -39,6 +74,7 @@ function SignUp() {
           </Link>
         </p>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
