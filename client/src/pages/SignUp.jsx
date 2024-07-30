a8da4316470fa2d2b68e16ced30b5b7db97f2e1b
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-30 20:53:04 +0300

    complete sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,46 @@
-function Signout() {
-  return <div>Signout</div>;
+import { Link } from "react-router-dom";
+
+function SignUp() {
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold my-7 text-center"> Sign Up</h1>
+      <form
+        action="
+      "
+        className="flex flex-col   gap-4"
+      >
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>
+          Have an account?{" "}
+          <Link to="/sign-in">
+            <span className="text-blue-700">Sign in</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Signout;
+export default SignUp;
