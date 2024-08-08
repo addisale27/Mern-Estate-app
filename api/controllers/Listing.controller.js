cfb32c6cd3c6fe9f3c952f3011e479623a33db7d
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-08-08 14:19:22 +0300

    complete update listing api route

--- api/controllers/Listing.controller.js
+++ api/controllers/Listing.controller.js
@@ -22,3 +22,19 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+export const updateListing = async (req, res, next) => {
+  const listing = Listing.findById(req.params.id);
+  if (!listing) return next(errorHandler(404, "listing not found!"));
+  if (listing.userRef !== req.user.id)
+    return next(errorHandler(401, "you can update your listings only!"));
+  try {
+    const updateListing = await Listing.findByAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json("Updated successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/Listing.route.js
+++ api/routes/Listing.route.js
@@ -1,8 +1,13 @@
 import express from "express";
 
-import { Create, deleteListing } from "../controllers/Listing.controller.js";
+import {
+  Create,
+  deleteListing,
+  updateListing,
+} from "../controllers/Listing.controller.js";
 import { verifyToken } from "../utils/VerifyUser.js";
 const router = express.Router();
 router.post("/create", verifyToken, Create);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 export default router;
